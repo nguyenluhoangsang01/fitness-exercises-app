7af961070e529af6f0b60b16534a4ca9ddd29a96
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-07-08 14:05:26 +0700

    Build routes and create Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { Box } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import NotFound from "./components/NotFound";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">fitness-exercises-app</div>;
+  return (
+    <Box
+      width="400px"
+      sx={{
+        width: {
+          xl: "1488px",
+        },
+      }}
+      m="auto"
+    >
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+
+      <Footer />
+    </Box>
+  );
 }
 
 export default App;
---
+++ src/pages/ExerciseDetail/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExerciseDetail = () => {
+  return <div>ExerciseDetail Page</div>;
+};
+
+export default ExerciseDetail;
