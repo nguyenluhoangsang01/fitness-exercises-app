b1155c2f2475bb18da725e77fbb23079a52fb9fc
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2022-07-09 12:14:12 +0700

    Add Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,21 @@
+import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
+import Logo from "../../assets/images/Logo-1.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const date = new Date();
+  const year = date.getFullYear();
+
+  return (
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="10px" alignItems={"center"} px="40px" pt="24px">
+        <img src={Logo} alt="Logo" width={"200px"} height={"40px"} />
+        <Typography variant="subtitle2" pb="40px" mt="20px">
+          © Copyright {year} nguyenluhoangsang.edu.vn. All rights reserved
+        </Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
